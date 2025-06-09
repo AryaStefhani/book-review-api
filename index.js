750c9f3a83c22a757387d64cdf56d6b8c7cd0a0e
AryaStefhani <hanpanka90@gmail.com>
2025-06-09 21:12:46 +0700

    index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 });
 
 // LISTEN using Railway's provided PORT or fallback to 3000
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
